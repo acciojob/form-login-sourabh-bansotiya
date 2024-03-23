e1c7dcca1e9e83ab85af2a09196ea65adda1e2bd
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-03-23 15:50:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function getFormvalue() {
+function getFormvalue(formId) {
     //Write your code here
+	const form = document.getElementById(formId);
+	const formData = new FormData(form);
+	const values = {};
 
+	for (const [key, value] of formData.entries()){
+		values[key] = value;
+	}
+return values;
 }
+document.getElementById('form1').addEventListener('submit', function(event){
+	event.preventDefault();
+
+	const formValues = getFormvalue('form1');
+	const message = `Name: ${formValues.fname}\nEmail: ${formValues.lname}`;
+
+	alert(message);
+});
