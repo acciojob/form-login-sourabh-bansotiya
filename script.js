e57acac3a6c848fdb673f3563983c88d67460786
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-03-28 12:41:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 
-        function getFormvalue(formId) {
-            const form = document.getElementById(formId);
-            const formData = new FormData(form);
-            const values = {};
+       function getFormvalue(formId) {
+    // Get the values from the form
+    let firstName = document.getElementById('fname').value;
+    let middleName = document.getElementById('mname').value;
+    let lastName = document.getElementById('lname').value;
 
-            for (const [key, value] of formData.entries()){
-                values[key] = value;
-            }
-            
-            const message = `${values.fname} ${values.lname}`;
-            alert(message);
-        }
+    // Construct the full name
+    let fullName = firstName;
+    
+    if (middleName) {
+        fullName += ` ${middleName}`;
+    }
+    
+    fullName += ` ${lastName}`;
+
+    // Display the full name using alert
+    alert(fullName);
+}
+
+        
 	
     
